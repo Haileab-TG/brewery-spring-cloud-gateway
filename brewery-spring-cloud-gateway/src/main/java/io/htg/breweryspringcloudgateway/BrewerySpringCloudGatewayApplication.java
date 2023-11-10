fcf06b3aa26cf11e010a02e358ac7edd18113d84
Haileab-TG <haileab.t.g@gmail.com>
2023-11-10 17:54:00 -0600

    Initial setup

---
+++ brewery-spring-cloud-gateway/src/main/java/io/htg/breweryspringcloudgateway/BrewerySpringCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.htg.breweryspringcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewerySpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewerySpringCloudGatewayApplication.class, args);
+	}
+
+}
